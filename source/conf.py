50f7863903eeaae8148ef9a3a682b68b07d3eef3
Franco Barberis <francobarberis@gmail.com>
2020-03-20 15:43:10 -0300

    Pyplan documentation 1.0

--- source/conf.py
+++ source/conf.py
@@ -17,7 +17,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Pyplan-doc'
+project = 'Pyplan Documentation'
 copyright = '2020, Pyplan'
 author = 'Pyplan'
 
@@ -56,10 +56,12 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-
+html_logo = '_static/pyplan_logo.jpg'
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+def setup(app):
+    app.add_css_file('pyplan.css')
